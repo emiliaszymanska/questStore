db6f679d7ff43c53c3e07c924b0b22ac06b8d103
Mat191126 <matprz11@gmail.com>
2020-09-21 16:03:32 +0200

    Add server in App.java, add QuestController class

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import com.company.controller.QuestController;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8001), 0);
 
+        server.createContext("/quest", new QuestController());
+
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server has started on port: " + server.getAddress().getPort());
     }
 }
---
+++ src/main/java/com/company/controller/QuestController.java
@@ -0,0 +1,70 @@
+package com.company.controller;
+
+import com.company.dao.QuestDao;
+import com.company.exceptions.ObjectNotFoundException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class QuestController implements HttpHandler {
+    private QuestDao questDao;
+
+    public QuestController (){
+        this.questDao = new QuestDao();
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        String response = "";
+
+        try {
+            switch (method){
+                case "GET":
+                    response = get(exchange);
+                    System.out.println(response);
+                    sendResponse(exchange, response, 200);
+                    break;
+
+                case "POST":
+                    break;
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            response = e.getMessage();
+            sendResponse(exchange, response, 404);
+        }
+    }
+
+    private String get(HttpExchange exchange) throws ObjectNotFoundException, JsonProcessingException {
+        String url = exchange.getRequestURI().getRawPath();
+        String[] actions = url.split("/");
+        System.out.println("URL: " + url + " | actions: " + Arrays.toString(actions));
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        if (actions.length == 3) {
+            int id = Integer.parseInt(actions[2]);
+            System.out.println(id);
+            return objectMapper.writeValueAsString(questDao.getById(id));
+        }
+
+        return objectMapper.writeValueAsString(questDao.getAll());
+    }
+
+    private void sendResponse(HttpExchange exchange, String response, int status) throws IOException {
+        if (status == 200) {
+            exchange.getResponseHeaders().put("Content-type", Collections.singletonList("application/json"));
+            exchange.getResponseHeaders().put("Access-Control-Allow-Origin", Collections.singletonList("*"));
+        }
+        exchange.sendResponseHeaders(status, response.getBytes().length);
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(response.getBytes());
+        outputStream.close();
+    }
+}
