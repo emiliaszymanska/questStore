fd2017573e0a896e56990997fbd5de1530affe9d
Emilia Szymanska <esz.emilia@gmail.com>
2020-10-19 09:52:48 +0200

    Add JUnit5 (jupiter) and Mockito dependencies to the pom file

--- src/test/java/com/company/AppTest.java
+++ src/test/java/com/company/AppTest.java
@@ -1,20 +1,9 @@
 package com.company;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
 }
