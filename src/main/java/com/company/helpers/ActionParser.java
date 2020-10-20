4201a6386460885f8bf18174f5d0847e97fe0915
Emilia Szymanska <esz.emilia@gmail.com>
2020-10-20 19:39:07 +0200

    Add Actions and ActionParser classes

---
+++ src/main/java/com/company/helpers/ActionParser.java
@@ -0,0 +1,8 @@
+package com.company.helpers;
+
+public class ActionParser {
+
+    public Actions fromURL(String url) {
+        return new Actions();
+    }
+}
---
+++ src/main/java/com/company/helpers/Actions.java
@@ -0,0 +1,19 @@
+package com.company.helpers;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class Actions {
+
+    public Optional<UUID> getUUID() {
+        return Optional.empty();
+    }
+
+    public String getEntity() {
+        return "wallet";
+    }
+
+    public String getOperation() {
+        return "anything";
+    }
+}
