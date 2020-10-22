6c29099e47c6c01a0873e3154a0f10e73ab93daa
Emilia Szymanska <esz.emilia@gmail.com>
2020-10-22 18:00:51 +0200

    RegisterService - add constructor

--- src/main/java/com/company/service/RegisterService.java
+++ src/main/java/com/company/service/RegisterService.java
@@ -13,8 +13,17 @@ import java.util.Map;
 
 public class RegisterService {
 
+    private UserDao userDao;
+    private StudentDao studentDao;
+    private ObjectMapper objectMapper;
+
+    public RegisterService() {
+        this.userDao = new UserDao();
+        this.studentDao = new StudentDao();
+        this.objectMapper = new ObjectMapper();
+    }
+
     public String createNewUser(User user, Map<String, String> formData) throws ObjectNotFoundException, JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
         User newUser = populateUserObjectWithData(user, formData);
 
         if (user instanceof Student) {
@@ -26,8 +35,6 @@ public class RegisterService {
     }
 
     private User populateUserObjectWithData(User user, Map<String, String> formData) throws ObjectNotFoundException {
-        UserDao userDao = new UserDao();
-
         user.setFirstName(formData.get("firstName"))
                 .setLastName(formData.get("lastName"))
                 .setTypeId(Integer.parseInt(formData.get("typeId")))
@@ -37,13 +44,11 @@ public class RegisterService {
                 .setActive(true);
 
         userDao.insert(user);
-        User newUser = userDao.getByEmailPassword(formData.get("email"), formData.get("password"));
 
-        return newUser;
+        return userDao.getByEmailPassword(formData.get("email"), formData.get("password"));
     }
 
     private User populateStudentObjectWithAdditionalData(Map<String, String> formData) throws ObjectNotFoundException {
-        StudentDao studentDao = new StudentDao();
         Student student = (Student) studentDao.getByEmailPassword(formData.get("email"), formData.get("password"));
 
         student.setModuleType(ModuleType.valueOf("PROGRAMMING_BASICS"))
