61fc357942df2d731dd425c7822b5648985c7f9a
Mat191126 <matprz11@gmail.com>
2020-11-03 14:26:00 +0100

    Tests refactor (imports optimalization)

--- src/test/java/com/company/controller/LoginControllerTest.java
+++ src/test/java/com/company/controller/LoginControllerTest.java
@@ -1,19 +1,15 @@
 package com.company.controller;
 
 import com.company.helpers.Parser;
-import com.company.model.user.User;
 import com.company.service.LoginService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
-
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoginControllerTest {
@@ -21,37 +17,37 @@ class LoginControllerTest {
     @Test
     void loginControllerHandle_test() throws IOException {
         //Arrange
-        HttpExchange exchange = Mockito.mock(HttpExchange.class);
+        HttpExchange exchange = mock(HttpExchange.class);
 
         Map<String, String> data = new HashMap<>();
         data.put("email", "email@gmail.com");
         data.put("password", "asd");
 
-        Parser parser = Mockito.mock(Parser.class);
-        Mockito.when(parser.parseFormData(exchange)).thenReturn(data);
+        Parser parser = mock(Parser.class);
+        when(parser.parseFormData(exchange)).thenReturn(data);
 
         SessionController sessionController = new SessionController();
 
-        ObjectMapper objectMapper = Mockito.mock(ObjectMapper.class);
-        LoginService loginService = Mockito.mock(LoginService.class);
+        ObjectMapper objectMapper = mock(ObjectMapper.class);
+        LoginService loginService = mock(LoginService.class);
 
         Headers headers = new Headers();
 
-        Mockito.when(exchange.getResponseHeaders()).thenReturn(headers);
-        Mockito.when(objectMapper.writeValueAsString(null)).thenReturn("response");
+        when(exchange.getResponseHeaders()).thenReturn(headers);
+        when(objectMapper.writeValueAsString(null)).thenReturn("response");
 
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        Mockito.when(exchange.getResponseBody()).thenReturn(byteArrayOutputStream);
+        when(exchange.getResponseBody()).thenReturn(byteArrayOutputStream);
 
         LoginController loginController = new LoginController(objectMapper, parser, sessionController, loginService);
         //Act
         loginController.handle(exchange);
         //Assert
         assertAll(
-                () -> Mockito.verify(objectMapper).writeValueAsString(null),
-                () -> Mockito.verify(parser).parseFormData(exchange),
+                () -> verify(objectMapper).writeValueAsString(null),
+                () -> verify(parser).parseFormData(exchange),
                 () -> assertTrue(sessionController.sessions.containsValue(null)),
-                () -> Mockito.verify(loginService).getUserByEmailAndPassword(data.get("email"), data.get("password")),
+                () -> verify(loginService).getUserByEmailAndPassword(data.get("email"), data.get("password")),
                 () -> assertEquals("true", headers.get("Access-Control-Allow-Credentials").get(0)),
                 () -> assertEquals("application/json", headers.get("Content-type").get(0)),
                 () -> assertEquals("http://localhost:63342", headers.get("Access-Control-Allow-Origin").get(0)),
--- src/test/java/com/company/service/LoginServiceTest.java
+++ src/test/java/com/company/service/LoginServiceTest.java
@@ -3,13 +3,13 @@ package com.company.service;
 import com.company.dao.StudentDao;
 import com.company.dao.UserDao;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoginServiceTest {
-    UserDao userDao = Mockito.mock(UserDao.class);
-    StudentDao studentDao = Mockito.mock(StudentDao.class);
+    UserDao userDao = mock(UserDao.class);
+    StudentDao studentDao = mock(StudentDao.class);
 
     @Test
     void getUserByEmailAndPassword_ReturnStudentWithAllData_WhenEmailAndPasswordGiven() {
